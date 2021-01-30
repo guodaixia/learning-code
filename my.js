dc05886e1dc01ec460a600e479bd52bc4ada90b9
guoxusheng <1764270154@qq.com>
2021-01-30 11:51:06 +0800

    loading first time

---
+++ my.js
@@ -0,0 +1,16 @@
+﻿var num = 10;
+console.log(typeof num);
+var str = 'pink';
+console.log(typeof str);
+var vari = undefined;
+console.log(typeof vari);
+var timer = null;
+//prompt 取过来的值是字符型
+
+var str = prompt('请输入你的年龄:');
+parseInt(str);
+console.log(parseInt(str));
+
+
+35.计算案例未完成
+40.作业
